9cac33d8369f8558875c654110b2cbfced7a312f
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-11-04 00:44:36 +0530

    Try the same(as prev) in react vite

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,8 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom function and react app</h1>
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
     <App />
+    <MyApp />
+    </>
 )
