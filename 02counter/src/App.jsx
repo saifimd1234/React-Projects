55539ebd2eb35c3702f4d769238a8e0c4b2ebdd5
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-11-04 11:56:41 +0530

    Basic counter function but it does not works.
    counter is defined with const that does not let it update the value.

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const counter = 15
+  
+  const addValue = () => {
+    counter = counter + 1
+    console.log(counter)
+  }
   return (
     <>
-      <h1>React course with saifi</h1>
-      <h2>Counter value: </h2>
-      <button>Add value</button>
+      <h1>React course with saifi {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button> {" "}
       <button>Remove value</button>
-      <footer>Footer: </footer>
+      <footer>Footer: {counter}</footer>
     </>
   )
 }
